3085d35389e3f325a71acff534c0decdc2dc7390
McSilva <microrepar@gmail.com>
2024-01-27 11:27:25 -0300

    configura o projeto para efetuar o deploy

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p96xyxkzv5_!1@cbzqqdvgk(7+($xx%c-e-$0wxi5eco$)(!u(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
